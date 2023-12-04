e191fb9ffb82b1e206bcae120ebb0e76e5f93a5d
Azake02 <azat.azamatov.02@bk.ru>
2023-12-04 22:20:23 +0600

    6 day

--- src/test/java/com/automation/utils/DriverUtils.java
+++ src/test/java/com/automation/utils/DriverUtils.java
@@ -2,22 +2,22 @@ package com.automation.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 
 import java.time.Duration;
 
 public class DriverUtils {
-    static WebDriver driver = DriverUtils.getDriver();
+    ChromeDriverService service = new ChromeDriverService.Builder().build();
+    WebDriver driver = new ChromeDriver(service);
 
     public static void createDriver() {
-        driver = new ChromeDriver();
+         WebDriver driver = new ChromeDriver();
         driver.get(ConfigProperties.getProperty("application.url"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
     public static WebDriver getDriver() {
-        return driver;
-       //fu mf githubdasdsa
-
+        return getDriver();
     }
 }
